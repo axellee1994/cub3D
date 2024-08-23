66ec70b71de9d8426a40dd926f9cb811dc1e7a16
Joshua Lai <jolai@c1r2s2.42singapore.sg>
2024-08-23 21:14:27 +0800

    added scene checks file, wip

---
+++ src/check_scene.c
@@ -0,0 +1,59 @@
+
+#include "cub3D.h"
+
+unsigned int	convert_rgb(int r, int g, int b)
+{
+	return (r << 16 | g << 8 | b);
+}
+
+int	check_color_val(char **arr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (arr[i])
+	{
+		while (arr[i][j])
+		{
+			if (!ft_isdigit(arr[i][j]))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	if (ft_atoi(arr[0]) > 255 || ft_atoi(arr[1]) > 255 || ft_atoi(arr[2]) > 255)
+		return (0);
+	return (1);
+}
+
+int	check_tex_file(char *file)
+{
+	if (ft_strncmp(&file[ft_strlen(file) - 4], ".xpm", 4) != 0)
+	{
+		ft_putstr_fd("Error\nInvalid texture file name:", STDERR_FILENO);
+		ft_putstr_fd(file, STDERR_FILENO);
+		ft_putstr_fd("File name must end in .xpm\n", STDERR_FILENO);
+		return (0);
+	}
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("Error\nFailed to open .xpm file\n", STDERR_FILENO);
+		return (0);	
+	}
+	close(fd);
+	return (1);
+}
+
+int	check_scene(t_scene *det)
+{
+	if (!check_color_val(det->floor) || !check_color_val(det->ceiling))
+		return (0);
+	if (!check_tex_file(det->north) || !check_tex_file(det->south) || 
+		!check_tex_file(det->east) || !check_tex_file(det->west))
+		return (0); 
+//	check map(flood fill on 0 and throw error if reach side or space?)
+	return (1);
+}
