934a1533403c7d907b9f545b135f14878289364b
Joshua Lai <jolai@c1r2s2.42singapore.sg>
2024-09-28 22:56:02 +0800

    Updated initialization and loading functions to exit on error

--- src/initialization.c
+++ src/initialization.c
@@ -6,7 +6,7 @@
 /*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 14:21:58 by axlee             #+#    #+#             */
-/*   Updated: 2024/09/28 21:06:26 by jolai            ###   ########.fr       */
+/*   Updated: 2024/09/28 22:51:18 by jolai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int	game_loop(t_mlx *mlx)
 {
 	if (!mlx || !mlx->ply || !mlx->ray || !mlx->dt)
 	{
-		ft_putstr_fd("Error: Uninitialized game data\n", STDERR_FILENO);
-		return (1);
+		load_error("Uninitialized game data", NULL, NULL, NULL);
+		free_and_exit(mlx);
 	}
 	hook(mlx, 0, 0);
 	cast_rays(mlx);
@@ -34,8 +34,8 @@ void	init_the_player(t_mlx *mlx)
 {
 	if (!mlx || !mlx->ply || !mlx->dt)
 	{
-		ft_putstr_fd("Error: Uninitialized player data\n", STDERR_FILENO);
-		return ;
+		load_error("Uninitialized player data", NULL, NULL, NULL);
+		free_and_exit(mlx);
 	}
 	mlx->ply->player_x = mlx->dt->player_x_position * TILE_SIZE + TILE_SIZE / 2;
 	mlx->ply->player_y = mlx->dt->player_y_position * TILE_SIZE + TILE_SIZE / 2;
@@ -58,7 +58,7 @@ void	start_the_game(t_data *dt, t_scene *scene)
 	mlx.ply = malloc(sizeof(t_player) + sizeof(t_ray));
 	if (!mlx.ply)
 	{
-		ft_putstr_fd("Error: Memory allocation failed\n", STDERR_FILENO);
+		load_error("Memory allocation failed", NULL, NULL, NULL);
 		free_and_exit(&mlx);
 	}
 	mlx.ray = (t_ray *)(mlx.ply + 1);
@@ -68,7 +68,6 @@ void	start_the_game(t_data *dt, t_scene *scene)
 	mlx.img.img = mlx_new_image(mlx.mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
 	init_image_buffer(&mlx);
 	init_textures(&mlx, scene);
-	free_scene(scene);
 	init_the_player(&mlx);
 	mlx.show_help = 0;
 	mlx_loop_hook(mlx.mlx, game_loop, &mlx);
