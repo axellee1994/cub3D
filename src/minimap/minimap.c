3c97f4c2a64b469ed12788fdd175a9996af00b4d
axellee1994 <axellee1994@gmail.com>
2024-09-28 15:42:19 +0800

    Adjusted border size for the minimap

--- src/minimap/minimap.c
+++ src/minimap/minimap.c
@@ -6,7 +6,7 @@
 /*   By: axlee <axlee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 15:11:13 by axlee             #+#    #+#             */
-/*   Updated: 2024/09/28 15:32:21 by axlee            ###   ########.fr       */
+/*   Updated: 2024/09/28 15:41:57 by axlee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,19 @@ static void	draw_minimap_tile_at(t_mlx *mlx, t_minimap *minimap, int x, int y)
 		draw_rectangle(mlx, pos, minimap->tile_size, MINIMAP_OUTSIDE_COLOR);
 }
 
-static void	draw_rectangle_outline(t_mlx *mlx, t_point pos, int size, int color)
+static void draw_rectangle_outline(t_mlx *mlx, t_point pos, int size, int color)
 {
-	int	i;
+    int i;
 
-	i = 0;
-	while (i < size)
-	{
-		draw_pixel(mlx, pos.x + i, pos.y, color);
-		draw_pixel(mlx, pos.x + i, pos.y + size - 1, color);
-		i++;
-	}
-	i = 0;
-	while (i < size)
-	{
-		draw_pixel(mlx, pos.x, pos.y + i, color);
-		draw_pixel(mlx, pos.x + size - 1, pos.y + i, color);
-		i++;
-	}
+    i = 0;
+    while (i < size)
+    {
+        draw_pixel(mlx, pos.x + i, pos.y, color);
+        draw_pixel(mlx, pos.x + i, pos.y + size - 1, color);
+        draw_pixel(mlx, pos.x, pos.y + i, color);
+        draw_pixel(mlx, pos.x + size - 1, pos.y + i, color);
+        i++;
+    }
 }
 
 static void	draw_minimap_recursive(t_mlx *mlx, t_minimap *minimap, int x, int y)
@@ -69,18 +64,19 @@ static void	draw_minimap_recursive(t_mlx *mlx, t_minimap *minimap, int x, int y)
 		draw_minimap_recursive(mlx, minimap, 0, y + 1);
 }
 
-void	draw_minimap(t_mlx *mlx)
+void draw_minimap(t_mlx *mlx)
 {
-	t_minimap	minimap;
-	t_point		border_pos;
-	int			border_size;
+    t_minimap minimap;
+    t_point border_pos;
+    int border_size;
 
-	init_minimap(mlx, &minimap);
-	draw_minimap_recursive(mlx, &minimap, 0, 0);
-	draw_minimap_player(mlx, &minimap);
-	draw_minimap_fov(mlx, &minimap);
-	border_pos.x = minimap.start_x - 1;
-	border_pos.y = minimap.start_y - 1;
-	border_size = minimap.width + 2;
-	draw_rectangle_outline(mlx, border_pos, border_size, MINIMAP_BORDER_COLOR);
+    init_minimap(mlx, &minimap);
+    draw_minimap_recursive(mlx, &minimap, 0, 0);
+    draw_minimap_player(mlx, &minimap);
+    draw_minimap_fov(mlx, &minimap);
+
+    border_pos.x = minimap.start_x - 1;
+    border_pos.y = minimap.start_y - 1;
+    border_size = minimap.width;
+    draw_rectangle_outline(mlx, border_pos, border_size, MINIMAP_BORDER_COLOR);
 }
